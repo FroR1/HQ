2a598d35f0a8efd3fda70be7009bc690c8f60b98
FroR1 <marmarsgai@yandex.ru>
2025-06-17 19:16:47 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -160,12 +160,8 @@ EOF
 # Функция настройки /etc/resolvconf.conf
 configure_resolv() {
     echo "Настройка /etc/resolvconf.conf..."
-    echo "name_servers=127.0.0.1" >> /etc/resolv.conf
-    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-    resolvconf -u
+    echo "nameserver 127.0.0.1" >> /etc/resolv.conf
     echo "Проверка интернета..."
-    cat /etc/resolv.conf
-    ping -c 4 77.88.8.8
     echo "/etc/resolvconf.conf настроен и проверка интернета выполнена."
 }
 
